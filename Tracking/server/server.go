bca55e9d6e555ac12892f41b1d72a754fbfab73b
izar99 <110042424+izar99@users.noreply.github.com>
2022-08-10 19:24:04 +0700

    Add files via upload

---
+++ Tracking/server/server.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"database/sql"
+	_ "mysql_master"
+)
+
+func Koneksi() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/etmsh")
+	if err != nil {
+		return nil, err
+
+	}
+
+	return db, nil
+
+}
